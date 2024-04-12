16a0ed32534f4992bfd7d26fdf0dde5147ddbed6
=Hardik Prajapati <=hardikmca3125@gmail.com>
2024-04-12 01:08:29 +0100

    changes in config file

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 //const Web3 = require("web3");
+const contractABI = require("./build/contracts/InventoryPayment.json");
 const { Web3 } = require('web3');
 
 const web3 = new Web3(process.env.WEB3_PROVIDER || "http://localhost:9545");
