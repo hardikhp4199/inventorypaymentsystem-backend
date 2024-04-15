e042231e259c572a51e6644b044c6b83d03ed982
=Hardik Prajapati <=hardikmca3125@gmail.com>
2024-04-16 00:01:06 +0100

    downgrade web3js issue

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 //const Web3 = require("web3");
 const contractABI = require("./build/contracts/InventoryPayment.json");
-const { Web3 } = require('web3');
+const Web3 = require("web3");
 
 const web3 = new Web3(process.env.WEB3_PROVIDER || "http://localhost:9545");
 
@@ -8,3 +8,16 @@ module.exports = {
     contractABI,
     web3
 };
+
+
+//  web3 version 4.7.0
+//const Web3 = require("web3");
+// const contractABI = require("./build/contracts/InventoryPayment.json");
+// const Web3 = require("web3");
+
+// const web3 = new Web3(process.env.WEB3_PROVIDER || "http://localhost:9545");
+
+// module.exports = {
+//     contractABI,
+//     web3
+// };
